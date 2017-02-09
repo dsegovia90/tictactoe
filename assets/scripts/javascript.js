5df34fb81c98a7a95df067df454ae2943c5d9d78
Daniel Segovia <daniel@dabici.mx>
2017-02-08 20:17:12 -0600

    Pushing to github pages

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 	$("#player-cpu").html("Player -> " + player + " CPU -> " + cpu);
 
 
-
 	var gridSystem = ["#0", "#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8"];
 	var clicked = [	0, 0, 0, 
 									0, 0, 0, 
